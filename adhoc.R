d990d5c30719ccd5863552de2940aaffa1418bd6
Tom White <tom.e.white@gmail.com>
2019-09-20 17:33:56 +0100

    Ad hoc rural school analysis

--- adhoc.R
+++ adhoc.R
@@ -48,3 +48,40 @@ schools_tidy %>%
   geom_jitter(width=0.05,alpha=0.2) +
   coord_flip()
 
+# Rural schools
+
+# Only primaries
+all_schools %>%
+  filter(year == '2018-19') %>%
+  filter(rural_school == 'Yes') %>%
+  group_by(school_type) %>%
+  tally()
+
+# Rural schools are small
+all_schools %>%
+  filter(year == '2018-19') %>%
+  filter(school_type == 'primary') %>%
+  ggplot(aes(x = rural_school, y = num_pupils)) +
+  geom_boxplot() +
+  geom_jitter(width = 0.1, size = 1) +
+  coord_flip()
+
+# Rural schools have lower occupancy
+all_schools %>%
+  filter(year == '2018-19') %>%
+  filter(school_type == 'primary') %>%
+  mutate(occupancy = 100.0 * num_pupils / capacity) %>%
+  ggplot(aes(x = rural_school, y = occupancy)) +
+  geom_boxplot() +
+  geom_jitter(width = 0.1, size = 1) +
+  coord_flip()
+
+# Small schools have lower occupancy
+all_schools %>%
+  filter(year == '2018-19') %>%
+  filter(school_type == 'primary') %>%
+  mutate(occupancy = 100.0 * num_pupils / capacity) %>%
+  ggplot(aes(x = num_pupils, y = occupancy)) +
+  geom_point() +
+  geom_smooth(method=lm)
+  

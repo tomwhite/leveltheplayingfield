bf8b4397ce8b24ea44fc5a0a39a154ead38344d8
Tom White <tom.e.white@gmail.com>
2019-10-15 21:39:48 +0100

    Population by age band

--- population.R
+++ population.R
@@ -1,9 +1,15 @@
-load_population_data <- function() {
+load_population_data <- function(csv = "data/population-estimates-by-local-authority-and-year.csv") {
   # Source: https://statswales.gov.wales/Catalogue/Population-and-Migration/Population/Estimates/Local-Authority/populationestimates-by-localauthority-year
-  read_csv("data/population-estimates-by-local-authority-and-year.csv") %>%
+  read_csv(csv) %>%
     rename(country = X3, local_authority = X4) %>%
     rename_all(gsub, pattern = '^Mid-year (.+)$', replacement = '\\1') %>%
     select(-c(X1, X2))
+}
+
+filter_to_wales_local_authorities <- function(all_population) {
+  all_population %>%
+    filter(!is.na(local_authority) & local_authority != 'Scotland' & local_authority != 'Northern Ireland') %>%
+    select(-c(country))
 }
 
 population <- load_population_data()
@@ -12,9 +18,7 @@ population_countries <- population %>%
   filter(!is.na(country) & (is.na(local_authority) | country == local_authority)) %>%
   select(-c(local_authority))
 
-population_wales <- population %>%
-  filter(!is.na(local_authority) & local_authority != 'Scotland' & local_authority != 'Northern Ireland') %>%
-  select(-c(country))
+population_wales <- filter_to_wales_local_authorities(population)
 
 # Plot of all LAs by year
 population_wales %>%
@@ -44,3 +48,27 @@ plot_population <- function(la, save_to_file=FALSE) {
 
 
 plot_population("Denbighshire")
+
+population_0_15 <- load_population_data("data/population-estimates-by-local-authority-and-year-0-15.csv") %>%
+  filter_to_wales_local_authorities() %>%
+  gather(year, population, -c(local_authority)) %>%
+  mutate(age = '0-15')
+population_16_64 <- load_population_data("data/population-estimates-by-local-authority-and-year-16-64.csv") %>%
+  filter_to_wales_local_authorities() %>%
+  gather(year, population, -c(local_authority)) %>%
+  mutate(age = '16-64')
+population_65_plus <- load_population_data("data/population-estimates-by-local-authority-and-year-65-and-over.csv") %>%
+  filter_to_wales_local_authorities() %>%
+  gather(year, population, -c(local_authority)) %>%
+  mutate(age = '65+')
+
+population_with_age <- population_0_15 %>%
+  union(population_16_64) %>%
+  union(population_65_plus)
+
+# See http://t-redactyl.io/blog/2016/01/creating-plots-in-r-using-ggplot2-part-4-stacked-bar-plots.html
+population_with_age %>%
+  filter(local_authority == 'Powys') %>%
+  ggplot(aes(y = population, x = year, fill = age)) +
+  geom_bar(stat = 'identity')
+
